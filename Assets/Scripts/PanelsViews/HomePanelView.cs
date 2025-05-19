b615125f15f2b42c83a535df1028b38ea68eed3f
Doğukan Gözüm <dogukangozum@hotmail.com>
2025-05-19 23:56:54 +0300

    home panel from views

--- Assets/Scripts/PanelsViews/HomePanelView.cs
+++ Assets/Scripts/PanelsViews/HomePanelView.cs
@@ -1,9 +1,22 @@
-﻿using Assets.Scripts.PanelService;
+﻿using System.Collections.Generic;
+using Assets.Scripts.PanelService;
+using Cysharp.Threading.Tasks;
+using Views;
 
 namespace PanelsViews
 {
-    public class HomePanelView : PanelBase
+    public class HomePanelView : PanelBase, IInitializableAsync<HomePanelView.Data>
     {
-        
+        public class Data
+        {
+            public List<AnnouncementView.Data> AnnouncementsData { get; }
+            public List<LeagueInfoView.Data> JoinedLeaguesData { get; }
+            public List<MatchInfoView.Data> UpcomingMatchesData { get; }
+        }
+
+        public UniTask InitAsync(Data data)
+        {
+            return UniTask.CompletedTask;
+        }
     }
 }
