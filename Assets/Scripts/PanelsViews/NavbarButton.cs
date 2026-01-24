21278218721084f16cfcda4faf25cf93e27f35bf
Doğukan Gözüm <dogukangozum@hotmail.com>
2026-01-24 15:20:38 +0300

    navbar tween updates

--- Assets/Scripts/PanelsViews/NavbarButton.cs
+++ Assets/Scripts/PanelsViews/NavbarButton.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -9,17 +10,26 @@ namespace PanelsViews
         public Button Button;
         [SerializeField] private Image icon;
         [SerializeField] private TMP_Text label;
-
+        [SerializeField] private Image topSelectedImage;
+        
         public void Convert(bool isActive, Color activeColor, Color inactiveColor)
         {
+            DOTween.Kill(this, true);
             if (icon != null)
             {
-                icon.color = isActive ? activeColor : inactiveColor;
+                icon.DOColor(isActive ? activeColor : inactiveColor, .2f).SetEase(Ease.OutQuad).SetId(this);
             }
-            
+
             if (label != null)
             {
-                label.color = isActive ? activeColor : inactiveColor;
+                label.DOColor(isActive ? activeColor : inactiveColor, .2f).SetEase(Ease.OutQuad).SetId(this);
+            }
+
+            if (topSelectedImage != null)
+            {
+                topSelectedImage.transform.DOScale(1f, 0.2f).From(0).SetEase(Ease.OutBack).SetId(this);
+                topSelectedImage.enabled = isActive;
+                topSelectedImage.color = activeColor;
             }
         }
     }
